d2a4b56731d993febd4fd1e5f028569512369567
DGWerlod <ddeanda333@gmail.com>
2018-02-17 14:13:03 -0800

    Added class room

--- game.py
+++ game.py
@@ -5,12 +5,16 @@ ctx = pygame.display.set_mode((gameW,gameH))
 pygame.display.set_caption("CircleGame")
 clock = pygame.time.Clock()
 
+class Room(object):
+	def __init__(self,xLength,yLength):
+		pass
+
 def close():
 	pygame.quit()
 	quit()
 
 
 def main():
-		pygame.display.update()
-		clock.tick(60)
+	pygame.display.update()
+	clock.tick(60)
 main()
