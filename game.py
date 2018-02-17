dfcfca80879e8baed20dc53463a60f51afad548d
DGWerlod <ddeanda333@gmail.com>
2018-02-17 15:28:20 -0800

    Added entity

--- game.py
+++ game.py
@@ -5,9 +5,19 @@ ctx = pygame.display.set_mode((200,100))
 pygame.display.set_caption("CircleGame")
 clock = pygame.time.Clock()
 
-class Room(object):
-	def __init__(self):
+class Entity(object):
+	def  __init__(self,x,y,w,h):
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+	def pos(self):
 		pass
+	def draw(self):
+		pygame.draw.rect(ctx,(0,0,0),(self.x,self.y,self.w,self.h))
+	def go(self):
+		self.pos()
+		self.draw()
 
 def close():
 	pygame.quit()
---
+++ room.py
@@ -0,0 +1,4 @@
+class Room(object):
+	def __init__(self,w,h,startX,startY,goalX,goalY,goalW,goalH):
+		self.w = w
+		self.h = h
