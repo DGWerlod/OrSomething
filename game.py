5082bd3068f3aac67c39b18685f1bcfb3a5b1068
DGWerlod <ddeanda333@gmail.com>
2018-02-17 14:03:33 -0800

    Initialize things

---
+++ game.py
@@ -0,0 +1,16 @@
+import pygame, math, random
+pygame.init()
+
+ctx = pygame.display.set_mode((gameW,gameH))
+pygame.display.set_caption("CircleGame")
+clock = pygame.time.Clock()
+
+def close():
+	pygame.quit()
+	quit()
+
+
+def main():
+		pygame.display.update()
+		clock.tick(60)
+main()
