74d83e9941a98310e586a3b9d6c853473fe701df
KathleenBaur <kathleen.susanne.baur@gmail.com>
2018-02-17 16:00:49 -0800

    colored albert

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import pygame, math, random
 pygame.init()
 
-ctx = pygame.display.set_mode((200,100))
-pygame.display.set_caption("CircleGame")
+ctx = pygame.display.set_mode((900,600))
+pygame.display.set_caption("Albert")
 clock = pygame.time.Clock()
 
 class Entity(object):
@@ -23,8 +23,15 @@ def close():
 	pygame.quit()
 	quit()
 
+anEntity = Entity(13,10,10,10)
 
 def main():
-	pygame.display.update()
-	clock.tick(2)
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				close()
+		ctx.fill((236,236,236))
+		anEntity.go()
+		pygame.display.update()
+		clock.tick(60)
 main()
